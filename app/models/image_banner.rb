9723fb1721c99647c7dd90dc6f2ca8a3b7cd2bb9
tsymbal <v.tsymbal@speople.pro>
2014-07-27 13:10:47 +0700

    change size of medium image

--- app/models/image_banner.rb
+++ app/models/image_banner.rb
@@ -1,10 +1,10 @@
 class ImageBanner < ActiveRecord::Base
   acts_as_banner
 
-  has_attached_file :file, styles: { medium:"300x300>",
-                                     thumb: "100x100>" },
+  has_attached_file :file, styles: { medium:"250x250#",
+                                     thumb: "100x100#" },
                            default_url: "/images/:style/missing.png"
 
-  validates_attachment_content_type :file, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :file,  content_type: /^image\/(png|gif|jpeg)/
   validates  :url, url: true
 end
