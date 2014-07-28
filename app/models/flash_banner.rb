ad0ff133b334e37b1cd3cc4325f79bb67b990c39
tsymbal <v.tsymbal@speople.pro>
2014-07-28 12:07:08 +0700

    added validation on content_type

--- app/models/flash_banner.rb
+++ app/models/flash_banner.rb
@@ -1,10 +1,9 @@
 class FlashBanner < ActiveRecord::Base
   acts_as_banner
 
-  has_attached_file :file, styles: { :medium => "300x300>",
-                                     :thumb => "100x100>" },
-                     default_url: "/images/:style/missing.png"
+  has_attached_file :file
 
-  validates_attachment_content_type :file, content_type: /\Aimage\/.*\Z/
+  validates :file, presence: true
+  validates_attachment_content_type :file, content_type: "application/vnd.adobe.flash.movie"
 
 end
