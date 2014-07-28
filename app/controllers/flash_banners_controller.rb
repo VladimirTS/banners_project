06151eba4636d12b6db0aeb756fdc81917d8cd17
tsymbal <v.tsymbal@speople.pro>
2014-07-28 12:08:12 +0700

    added posibility create flash banner

--- app/controllers/flash_banners_controller.rb
+++ app/controllers/flash_banners_controller.rb
@@ -1,12 +1,11 @@
 class FlashBannersController < ApplicationController
+  before_action :find_banner, only: [:show, :edit, :update, :destroy]
 
   def index
 
   end
 
-  def show
-    @flash_banner = FlashBanner.find(params[:id])
-  end
+  def show; end
 
   def new
     @flash_banner = FlashBanner.new
@@ -16,20 +15,20 @@ class FlashBannersController < ApplicationController
     @flash_banner = FlashBanner.new(flash_banner_params)
 
     if @flash_banner.save
+      add_position_to_banner
       redirect_to @flash_banner
     else
       render "new"
     end
   end
 
-  def edit
-    @flash_banner = FlashBanner.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @flash_banner = FlashBanner.find(params[:id])
+    remove_old_positions(@flash_banner)
 
-    if @flash_banner.update_attributes(params[:banner])
+    if @flash_banner.update_attributes(flash_banner_params)
+      add_position_to_banner
       redirect_to @flash_banner
     else
       render "edit"
@@ -37,17 +36,37 @@ class FlashBannersController < ApplicationController
   end
 
   def destroy
-    @flash_banner = ImageBanner.find(params[:id])
     @flash_banner.destroy
     redirect_to "/admin"
   end
 
   private
 
+    def find_banner
+      begin
+        @flash_banner = FlashBanner.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        redirect_to "/"
+      end
+    end
+
     def flash_banner_params
-      params.require(:flash_banner).permit(:controller,    :position,
-                                           :property_type, :deal_kind,
-                                           :deal_direction, :action,
-                                           :file)
+      params.require(:flash_banner).permit(:controller, :property_type,
+                                           :deal_kind,  :deal_direction,
+                                           :action,     :file, :positions)
+    end
+
+    def add_position_to_banner
+      if params[:flash_banner][:positions]
+        params[:flash_banner][:positions].each do |position|
+          Position.create(banner_id: @flash_banner.id, value: position)
+        end
+      end
+    end
+
+    def remove_old_positions(banner_type)
+      banner_type.banner.positions.each do |position|
+        position.destroy
+      end
     end
 end
