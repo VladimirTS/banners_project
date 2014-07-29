f97652160798c80a6e7a27c04d20c0dc6227365d
tsymbal <v.tsymbal@speople.pro>
2014-07-29 12:48:14 +0700

    refactor model

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -14,38 +14,41 @@ class Banner < ActiveRecord::Base
       # "deal_kind"=>"sale", "deal_direction"=>"offer"}
 
       params[:controller] = params[:controller].camelize
-      @all_banners    = Banner.where(params.except(:action))
+      banners = Banner.where(params.except(:action))
+
       @cur_action     = params[:action]
       @cur_controller = params[:controller].camelize
 
       # return arrays: top, middle, right, height
-      get_positions_array
+      positions = get_positions_array(banners)
 
-      @banner_positions = { top:    get_rand_banner(@pos_top),
-                            middle: get_rand_banner(@pos_middle),
-                            right:  get_rand_banner(@pos_right),
-                            bottom: get_rand_banner(@pos_bottom) }
+      @banner_positions = { top:    get_rand_banner(positions[:top]),
+                            middle: get_rand_banner(positions[:middle]),
+                            right:  get_rand_banner(positions[:right]),
+                            bottom: get_rand_banner(positions[:bottom]) }
     end
 
-    def get_positions_array
-      @pos_top, @pos_middle, @pos_right, @pos_bottom  = [], [], [], []
+    def get_positions_array(banners)
+      @positions = { top: [], middle: [], right: [], bottom: [] }
 
-      @all_banners.each do |banner|
+      banners.each do |banner|
         if (@cur_controller == banner.controller) && (banner.action.nil? || banner.action == @cur_action)
-          positions = banner.positions
-          positions.each do |pos|
+          banner_positions = banner.positions
+          banner_positions.each do |pos|
             if pos.value == "top"
-              @pos_top << banner
+              @positions[:top]  << banner
             elsif pos.value == "middle"
-              @pos_middle << banner
+              @positions[:middle] << banner
             elsif pos.value == "right"
-              @pos_right << banner
+              @positions[:right] << banner
             else
-              @pos_bottom << banner
+              @positions[:bottom] << banner
             end
           end
         end
       end
+
+      @positions
     end
 
     def get_rand_banner(banners)
